1a1f5e986956ee438ca54053d1a646e796ddc013
AngryReaper <angryreaper@gmail.com>
2019-01-02 13:43:22 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <div>
+            <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        </div>
+        <div>
+            <NavLink to="/create" activeClassName="is-active">Create expense</NavLink>
+        </div>
+        <div>
+            <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+        </div>
+    </header>
+);
+
+export default Header;
