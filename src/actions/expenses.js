0bb7a0a9a02afde48c2b1c3127f45a3b6d775077
Viacheslav Perfilkin <perfilkin.v@iMac-Viacheslav.local>
2019-01-29 14:59:16 +0300

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -33,6 +33,19 @@ export const removeExpense = ({id} = {}) => ({
     id
 });
 
+export const startRemoveExpense = ({id} = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove()
+            .then(() => {
+                dispatch(removeExpense({
+                    id
+                }))
+            }, (e) => {
+                console.log("Error", e)
+            })
+    }
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
